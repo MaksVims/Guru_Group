7527f3d500974b863f753f8f3346e451e33a8e06
Maksim Smetanin <maks.s923@mail.ru>
2022-08-05 13:30:28 +0500

    feat: create theme and global style

---
+++ src/style/theme.tsx
@@ -0,0 +1,26 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  media: {
+    small: '360px',
+    mobile: '480px',
+    tablets: '768px',
+    laptops: '1024px',
+  },
+  fontSize: {
+    small: '12px',
+    title: '22px',
+    default: '14px',
+  },
+  colors: {
+    white: '#ffffff',
+    accent: '#00A0AB',
+    dark: '#2c2c2cbd',
+    darkGrey: '#5A5A5A',
+    grey: '#C7C7C7',
+    textGrey: '#8F8F8F',
+    bg: {
+      seen: '#FFF6A5',
+    },
+  },
+}
